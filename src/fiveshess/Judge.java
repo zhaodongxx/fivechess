f120a33669dc429cd00016d442b7f50937097fcc
赵东 <zhaodongxx@outlook.com>
2018-08-13 09:43:19 +0800

    fix

--- src/fiveshess/Judge.java
+++ src/fiveshess/Judge.java
@@ -1,7 +1,10 @@
 package fiveshess;
 
+import java.awt.*;
+
 /**
  * 裁判
+ *
  * @author zhaodong zhaodongxx@outlook.com
  * @version v1.0
  * @since 2018/7/17 17:26
@@ -9,4 +12,24 @@ package fiveshess;
 public class Judge {
 
 //    public  isvictory
+    this.space =space;
+    this.grids =grids;
+    this.rad =space /2;
+
+    setBackground(new Color(200, 100,50));
+
+    setSize(760,760);
+    startMenuItem.addActionListener(startHandler);
+    exitMenuItem.addActionListener(exitHandler);
+    aboutMenuItem.addActionListener(aboutHandler);
+
+    addMouseListener(playChessHandler);
+
+    chessJMenuBar.add(optMenu);
+    chessJMenuBar.add(helpMenu);
+    optMenu.add(startMenuItem);
+    optMenu.add(exitMenuItem);
+    helpMenu.add(aboutMenuItem);
+//        add(BorderLayout.EAST, new JButton("EAST"));
+
 }
